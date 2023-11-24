95e676738ef9bae3e723284d996d03e2702ccc13
riemer1818 <riemer.vandervliet@Live.nl>
2023-11-24 12:14:52 +0800

    added ResNet implementation

--- models/networks.py
+++ models/networks.py
@@ -1,4 +1,5 @@
 import torch
+from torchvision import models
 import torch.nn as nn
 from torch.nn import init
 import functools
@@ -615,46 +616,53 @@ class PixelDiscriminator(nn.Module):
         """Standard forward."""
         return self.net(input)
 
+class ResNetLoss(nn.Module):
+    def __init__(self, gpu_ids):
+        super(ResNetLoss, self).__init__()
+        self.Resnet = ResNet().cuda()
+        self.criterion = nn.L1Loss()
+        self.weights = [1.0/32, 1.0/16, 1.0/8, 1.0/4, 1.0] # how did they get these values? 
 
-class Vgg19(torch.nn.Module):
+    def forward(self, x, y):
+        # x_vgg, y_vgg = self.vgg(x), self.vgg(y)
+        x_ResNet, y_ResNet = self.Resnet(x), self.Resnet(y)
+        loss = 0
+        for i in range(len(x_ResNet)):
+            loss += self.weights[i] * \
+                self.criterion(x_ResNet[i], y_ResNet[i].detach())
+        return loss
+
+class ResNet(torch.nn.Module):
     """
-    A modified VGG19 network for feature extraction.
-
+    A modified ResNet network for feature extraction.
+    
     Args:
         requires_grad (bool): Whether to require gradients for the network parameters. Default: False.
     """
 
-    def __init__(self, requires_grad=False):
-        super(Vgg19, self).__init__()
-        vgg_pretrained_features = models.vgg19(pretrained=True).features
+    def __init__(self,  required_grad=False):
+        super(ResNet, self).__init__()
+        resnet_pretrained_features = models.resnet18(pretrained=True).features
         self.slice1 = torch.nn.Sequential()
         self.slice2 = torch.nn.Sequential()
         self.slice3 = torch.nn.Sequential()
         self.slice4 = torch.nn.Sequential()
         self.slice5 = torch.nn.Sequential()
-        for x in range(2):
-            self.slice1.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(2, 7):
-            self.slice2.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(7, 12):
-            self.slice3.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(12, 21):
-            self.slice4.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(21, 30):
-            self.slice5.add_module(str(x), vgg_pretrained_features[x])
-        if not requires_grad:
+        for x in range(3):
+            self.slice1.add_module(str(x), resnet_pretrained_features[x])
+        for x in range(3, 5):
+            self.slice2.add_module(str(x), resnet_pretrained_features[x])
+        for x in range(5, 6):
+            self.slice3.add_module(str(x), resnet_pretrained_features[x])
+        for x in range(6, 7):
+            self.slice4.add_module(str(x), resnet_pretrained_features[x])
+        for x in range(7, 8):
+            self.slice5.add_module(str(x), resnet_pretrained_features[x])
+        if not required_grad:
             for param in self.parameters():
                 param.requires_grad = False
 
-    def forward(self, X):
-        h_relu1 = self.slice1(X)
-        h_relu2 = self.slice2(h_relu1)
-        h_relu3 = self.slice3(h_relu2)
-        h_relu4 = self.slice4(h_relu3)
-        h_relu5 = self.slice5(h_relu4)
-        out = [h_relu1, h_relu2, h_relu3, h_relu4, h_relu5]
-        return out
-    
+
 class VGGLoss(nn.Module):
     def __init__(self, gpu_ids):
         super(VGGLoss, self).__init__()
@@ -670,6 +678,7 @@ class VGGLoss(nn.Module):
                 self.criterion(x_vgg[i], y_vgg[i].detach())
         return loss
 
+
 class Vgg19(torch.nn.Module):
     """
     A modified VGG19 network for feature extraction.
@@ -708,4 +717,4 @@ class Vgg19(torch.nn.Module):
         h_relu5 = self.slice5(h_relu4)
         out = [h_relu1, h_relu2, h_relu3, h_relu4, h_relu5]
         return out
-    
+
