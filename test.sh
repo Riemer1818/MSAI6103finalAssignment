764c91b393042721015f0354a4eab830a9d083ad
#VAN DER VLIET RIEMER PIETER# <riemerpi001@SCSEGPU-TC2.cm.cluster>
2023-11-25 14:57:17 +0800

    updated shells

--- test.sh
+++ test.sh
@@ -12,4 +12,7 @@
 module load anaconda3/23.5.2
 eval "$(conda shell.bash hook)"
 conda activate pytorch-CycleGAN-and-pix2pix
+python test.py --dataroot ./datasets/facades --name facades_pix2pix_L1 --model pix2pix --direction BtoA
+python test.py --dataroot ./datasets/facades --name facades_pix2pix_MobileNet --model pix2pix --direction BtoA
 python test.py --dataroot ./datasets/facades --name facades_pix2pix_ResNet --model pix2pix --direction BtoA
+python test.py --dataroot ./datasets/facades --name facades_pix2pix_VGG --model pix2pix --direction BtoA
--- trainMobileNet.sh
+++ trainMobileNet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=SCSEGPU_M1
-#SBATCH --qos=q_amsai
+#SBATCH --partition=SCSEGPU_M2
+#SBATCH --qos=q_dmsai
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --mem=1G
